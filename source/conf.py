0a6445a21b001d53299c2e04dfe00cd07d9054c3
trigaten <sanderschulhoff@gmail.com>
2022-10-27 01:00:23 -0400

    feat: expand page width

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,10 @@ author = 'Sander Schulhoff'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_size',
 ]
+
+sphinx_rtd_size_width = "90%"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
