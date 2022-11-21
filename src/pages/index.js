08f4a42b79a32cd220df26fc8df507a9a4022d52
trigaten <sanderschulhoff@gmail.com>
2022-11-21 00:35:54 -0500

    feat: update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Learn Prompt Engineering">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
