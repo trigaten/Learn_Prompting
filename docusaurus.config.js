b21e99b97565fcefa5f6cf33425ea4658539b85f
trigaten <sanderschulhoff@gmail.com>
2022-12-06 10:40:09 -0500

    fix: monkeypatch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,11 +73,6 @@ async function createConfig() {
         integrity:
           'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
         crossorigin: 'anonymous',
-      },
-      {
-        href: 'https://embed.trydyno.com/embedder.css',
-        type: "text/css",
-        crossorigin: 'anonymous',
       }
     ],
     themeConfig:
