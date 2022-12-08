714776e070150eb8eb861b967e47a1693455c442
trigaten <sanderschulhoff@gmail.com>
2022-12-08 17:45:40 -0500

    fix: fixed window not defined errors with redirects

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from '@docusaurus/Link';
 
 export default function Discord() {
     let url = "https://discord.gg/4Kq46MKFpH"
-    window.location.href = url;
+    useEffect(() => {
+        window.location.href = url;
+    }, []);
 
     return (
         <p>Redirecting to link..., please click <Link href={url} target="_self">here</Link> if not redirected</p>
