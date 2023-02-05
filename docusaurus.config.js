d0babe0ea12bf1a4bfd1e5a4899b81ed468cd64a
Fumikazu Fujiwara | Freddie <github@ze.gs>
2023-02-05 20:18:22 +0900

    add 'ja'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ async function createConfig() {
     // to replace "en" with "zh-Hans".
     i18n: {
       defaultLocale: 'en',
-      locales: ['en', 'es'],
+      locales: ['en', 'es' ,'ja'],
     },
   
     presets: [
