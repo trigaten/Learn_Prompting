60093d21708535214361338a0ad96c2a73bd995d
anaumkhan <anaumkhan10@gmail.com>
2023-04-27 17:33:19 -0400

    fix: include key

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import ClassicPadding from "./layouts/ClassicPadding";
 import { RxArrowTopRight } from "react-icons/rx";
 
-
 function Footer() {
   const rightLinks = [
     {
@@ -27,8 +26,11 @@ function Footer() {
     <ClassicPadding className="bg-gray/300 py-16 border-t-[gray/200] border flex flex-col md:flex-row justify-between font-vietnam tracking-tight text-default">
       <div className="hidden md:flex">© 2023 Learn Prompting</div>
       <div className="flex gap-4">
-        {rightLinks.map((link) => (
-          <div className="flex items-center text-default hover:text-dark/500">
+        {rightLinks.map((link, idx) => (
+          <div
+            className="flex items-center text-default hover:text-dark/500"
+            key={idx}
+          >
             <a
               href={link.link}
               className=" transition-all mr-1 font-vietnam tracking-tight"
@@ -40,7 +42,6 @@ function Footer() {
         ))}
       </div>
       <div className="flex md:hidden mt-6">© 2023 Learn Prompting</div>
-
     </ClassicPadding>
   );
 }
