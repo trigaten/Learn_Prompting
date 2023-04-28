10e2b7de9a84baa226494c42c2ba524fd332267e
trigaten <sanderschulhoff@gmail.com>
2023-04-28 01:53:37 -0400

    feat: email sign up

--- src/pages/subsections/landingpage/Newsletter.js
+++ src/pages/subsections/landingpage/Newsletter.js
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
+
+class NewsletterSignupForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "", submitted: false };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://hooks.zapier.com/hooks/catch/15160672/343aepr/", {
+      method: "POST",
+      body: JSON.stringify({ email: this.state.email }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ submitted: true });
+      });
+  }
+
+  render() {
+    const { submitted } = this.state;
+
+    return (
+      <div
+        className={`largecallout w-screen flex flex-col justify-center items-center text-white md:p-20 pt-28 pb-28 ${
+          submitted ? "form-submitted" : ""
+        }`}
+      >
+        {submitted ? (
+          <div className="font-semibold tracking-tight text-4xl md:text-6xl flex flex-col items-center">
+            <div>🎉</div>
+            <div>Signed up!</div>
+            <div>🙌</div>
+          </div>
+        ) : (
+          <>
+            <div className="font-semibold tracking-tight text-4xl md:text-6xl">
+              Join the Newsletter
+            </div>
+            <div className="text-center text-white opacity-60 text-sm font-vietnam font-light tracking-tight px-16 md:px-28 pt-10">
+              Stay up to date with the latest content delivered to your inbox.
+            </div>
+            <div className="pt-10 flex flex-row">
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                  required
+                  style={{ color: "black" }}
+                  placeholder="email@learnprompting.org"
+                  className="rounded-tl-md rounded-bl-md px-6 py-2  bg-white text-dark/500 text-sm font-medium tracking-tight ring-0 focus:outline-none w-[250px] md:w-[450px] focus:ring-0"
+                />
+                <button
+                  type="submit"
+                  style={{ width: "100%" }}
+                  className="rounded-tr-md rounded-br-md bg-[#2A5F66] flex items-center font-semibold justify-center px-6 py-2 hover:bg-[#1F4A52] active:bg-[#2D6370]"
+                >
+                  Sign Up
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 function Newsletter() {
-  return (
-    <div className="largecallout w-screen flex flex-col justify-center items-center text-white  md:p-20 pt-28 pb-28">
-      <div className="font-semibold tracking-tight text-4xl md:text-6xl">
-        Join the Newsletter
-      </div>
-      <div className="text-center text-white opacity-60 text-sm font-vietnam font-light tracking-tight px-16 md:px-28 pt-10">
-        Stay up to date with the latest content delivered to your inbox.
-      </div>
-      <div className="pt-10 flex flex-row">
-        <input
-          type="text"
-          placeholder="email@learnprompting.org"
-          className="rounded-tl-md rounded-bl-md px-6 py-2  bg-white text-dark/500 text-sm font-medium tracking-tight ring-0 focus:outline-none w-[250px] md:w-[450px] focus:ring-0"
-        />
-        <div className="rounded-tr-md rounded-br-md bg-[#2A5F66] flex items-center font-semibold justify-center px-6 py-2">
-          Sign Up
-        </div>
-      </div>
-    </div>
-  );
+  return <NewsletterSignupForm />;
 }
 
 export default Newsletter;
