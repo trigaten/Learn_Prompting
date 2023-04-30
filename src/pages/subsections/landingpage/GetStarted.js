cbeed79b7c670515297ea21796a4f9abcbc0fb73
trigaten <sanderschulhoff@gmail.com>
2023-04-30 09:00:41 -0400

    fix: improve centering of text above astronaut

--- src/pages/subsections/landingpage/GetStarted.js
+++ src/pages/subsections/landingpage/GetStarted.js
@@ -8,7 +8,7 @@ function GetStarted() {
     <ClassicPadding
       className={"flex flex-col md:flex-row gap-4 justify-between md:py-24"}
     >
-      <div className="flex items-center md:w-[60%]">
+      <div className="items-center md:w-[60%]">
         <div className="text-center md:text-left text-5xl md:text-6xl font-vietnam font-medium tracking-tighter pt-16">
           Get Started.
           <div className="text-center md:text-left text-default text-sm font-vietnam font-light tracking-tight pt-4 px-12 md:px-0">
