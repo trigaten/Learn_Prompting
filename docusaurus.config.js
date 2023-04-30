452380da12a0fca7b486ec286622f35445995aac
trigaten <sanderschulhoff@gmail.com>
2023-04-30 10:27:32 -0400

    feat: redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,6 +39,10 @@ async function createConfig() {
               to: '/docs/basics/formalizing',
               from: '/docs/basics/standard_prompt',
             },
+            {
+              to: '/docs/tooling/IDEs/intro',
+              from: '/docs/IDEs/intro',
+            },
           ],
         },
       ],
