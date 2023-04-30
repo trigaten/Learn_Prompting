4abfe582cc1856673dfd6ff2ac9126f53a75303b
trigaten <sanderschulhoff@gmail.com>
2023-04-30 19:54:29 -0400

    New site (#668)
    * Test
    
    * FAQ Page Test
    
    * feat: add questions
    
    * feat: more faq
    
    * feat: more faq
    
    * feat: more faqs
    
    * added answers
    
    * saving for u
    
    * fix: include key
    
    * fix: improve hero text wrapping
    
    * feat: real links
    
    * fix: links
    
    * fix: temp links
    
    * fix: temp link fix
    
    * feat: email sign up
    
    * feat: use array
    
    * Blog (#670)
    
    * feat: blog/comp info
    
    * feat: small clarifications
    
    * F
    
    * featL updarte blog
    
    * feat: huge prompt hacking update
    
    * fix: \hat problems
    
    * feat: blog
    
    * feat: add blog to navbar
    
    * New Landing Page Update
    
    * Added the blog tab and fixed the FAQ heading
    
    * fix: merge conflict
    
    * fix: bottom astronaut image
    
    * fix: changed the icon layout
    
    * fix: lock image
    
    * fix: links
    
    * fix: stop tailwind from effecting docs
    
    * fix: added the lock svg to the competition panel
    
    * feat: improve dual nav
    
    * Fixed the spacing for the hero layout
    
    * fix: dont display all links
    
    * fix: spacing of the headers
    
    * fix: color to black
    
    * fix: change contact us color to grey
    
    * fix: add dalle caption
    
    * fix: color black
    
    * Fixed the competition layout spacing to be aligned
    
    * fix: sitting astronaut
    
    * fix: dark mode no longer affects landing page
    
    * fix: formatting
    
    * fix: fixed the position of the lock and text in the competition panel and changed the spacing the the get started section
    
    * feat: move astronaut to right
    
    * fix: get started
    
    * feat: mobile optimizations
    
    * made sure the png is center aligned in the competition panel
    
    * fix: changed the sizing of the get started subheading
    
    * fix: mobile optimizations
    
    * fix: changed the panels to be svgs
    
    * merge conflict
    
    * added the scrolling functionality to switch panels for the beginner, intermediate, advanced and applications level
    
    * added the carousel for the company logos to be displayed as sponsors for the HackAPrompt competition
    
    * fix: improve astronaut centering
    
    * fix: improve word spacing
    
    * fix: improve centering of text above astronaut
    
    * fix: faq title spacing
    
    * fix: changed the carousel so that it would not reset and instead just continously cycle through the logos
    
    * fix: changed the sizing of the subheading of the hero
    
    * fix: the arrow position for the FAQ page
    
    * fix: padding
    
    * fix: added line breaks to all displays for the subheadings
    
    * fix: spacing between the stats
    
    * fix: imports and deleting the scrollingtext component
    
    * fix: get started section updated to include a clear subheading
    
    * fix: the size and spacing of the logos and the stats
    
    * fixed the sizing the get started section
    
    * fix: competition subheading size
    
    * fix: colors in dark mode
    
    * fix: more colors in darkmode
    
    * fix: the overflow problem with the panels on the hero and the color of the intermediate, advanced and applications button when selected
    
    * updated the docusaurus config file
    
    ---------
    
    Co-authored-by: anaumkhan <anaumkhan10@gmail.com>
    Co-authored-by: Luke Currier <115105781+Dastardi@users.noreply.github.com>

---
+++ src/pages/subsections/landingpage/Join.js
@@ -0,0 +1,157 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import { motion } from "framer-motion";
+import ClassicPadding from "@site/src/components/layouts/ClassicPadding";
+import StatPoint from "@site/src/components/StatPoint";
+import LargeCallout from "@site/src/components/layouts/LargeCallout";
+import Button from "@site/src/components/Button";
+import { RxArrowTopRight } from "react-icons/rx";
+import Arthur from "@site/static/img/arthur_logo.png";
+import HuggingFace from "@site/static/img/huggingface_logo.png";
+import HumanLoop from "@site/static/img/humanloop_logo.png";
+import Preamble from "@site/static/img/preamble_logo.png";
+import Scale from "@site/static/img/scale_logo.png";
+import Snorkel from "@site/static/img/snorkel_logo.png";
+import Stability from "@site/static/img/stability_logo.png";
+import Towards from "@site/static/img/towards_logo.png";
+import Trustible from "@site/static/img/trustible_logo.png";
+import Voiceflow from "@site/static/img/voiceflow_logo.png";
+import FiscalNote from "@site/static/img/fiscalnote_logo.png";
+
+function Join() {
+  const stats = [
+    {
+      header: "700K+",
+      subtitle: "People Learning",
+    },
+    {
+      header: "36K+",
+      subtitle: "Discord Members",
+    },
+    {
+      header: "60+",
+      subtitle: "Content Modules",
+    },
+    {
+      header: "9+",
+      subtitle: "Languages",
+    },
+  ];
+
+  const brands = [
+    { name: "Scale", logo: Scale, link: "https://scale.com/" },
+    { name: "Stability", logo: Stability, link: "https://stability.ai/" },
+    { name: "HuggingFace", logo: HuggingFace, link: "https://huggingface.co/" },
+    { name: "HumanLoop", logo: HumanLoop, link: "https://humanloop.com/" },
+    { name: "Preamble", logo: Preamble, link: "https://www.preamble.com/" },
+    { name: "Voiceflow", logo: Voiceflow, link: "https://www.voiceflow.com/" },
+    { name: "Arthur", logo: Arthur, link: "https://www.arthur.ai/" },
+    { name: "Snorkel", logo: Snorkel, link: "https://snorkel.ai/" },
+    { name: "Towards", logo: Towards, link: "https://towardsai.net/" },
+    { name: "Trustible", logo: Trustible, link: "https://www.trustible.ai/" },
+    { name: "FiscalNote", logo: FiscalNote, link: "https://fiscalnote.com/" },
+  ];
+
+  return (
+    <div className="w-screen font-vietnam">
+      <div className="text-center text-2xl md:text-5xl font-vietnam font-semibold md:font-medium tracking-tighter px-4 md:px-32 md:pt-15 lg:px-[200px]">
+        <span style={{color:"black"}}>Join our global community.</span>
+        <br />
+        <span style={{color:"black"}}>Elevate your prompting skills.</span>
+        <div className="text-center text-default text-sm font-vietnam font-light tracking-tight px-4 md:px-28 pt-10">
+          Become part of a worldwide network of learners from various
+          industries, all mastering {window.innerWidth >= 768 && <br />} the
+          skill of effectively engaging with AI using our curriculum
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-8 md:gap-0 md:flex-row items-center justify-between px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-32 text-transparent bg-clip-text bg-gradient-to-r from-[#005046] to-[#027F75] pt-14 pb-10 max-w-screen-xl mx-auto">
+        {stats.map((stat, i) => (
+          <StatPoint key={i} header={stat.header} subtitle={stat.subtitle} />
+        ))}
+      </div>
+
+      <LargeCallout className={"pt-20 tracking-tighter"}>
+        <div
+          id={"competition"}
+          className="flex gap-4 justify-around flex-col md:flex-row"
+        >
+          <div className="md:w-[45%] md:pl-28 md:ml-12">
+            <div className="opacity-81 text-center md:text-left text-lg">
+              Introducing The
+            </div>
+            <div className="mt-8 text-4xl tracking-tighter text-center md:text-left">
+              <span className="font-bold">HackAPrompt</span> Competition
+            </div>
+            <div className="opacity-75 font-light pt-8 md:text-left text-center px-4 md:px-0 leading-6">
+              A beginner-friendly competition that will challenge you to beat 7
+              levels of prompt hacking defenses. Inject, leak and defeat the
+              sandwich defense to claim your share of over $37,500 in prizes.
+              Ready to trick the AI? Hack our prompts anytime between May 5th
+              and May 26th!
+            </div>
+            <div className="flex pt-8 justify-center md:justify-start">
+              <Button
+                onClick={() =>
+                  window.open(
+                    "https://www.aicrowd.com/challenges/hackaprompt-2023",
+                    "_blank",
+                    "noopener noreferrer"
+                  )
+                }
+                text={"Register Now"}
+                type={"white"}
+                icon={
+                  <RxArrowTopRight
+                    className="inline-block text-green/300"
+                    style={{ position: "relative", top: "3px" }}
+                  />
+                }
+              />
+            </div>
+          </div>
+          <div
+            className="w-[550px] h-[325px] mr-[-100px] md:flex hidden"
+            style={{ paddingTop: "20px", marginTop: "-20px" }}
+          >
+            <img
+              src={require("@site/static/img/lock.png").default}
+              alt="Example banner"
+            />
+          </div>
+        </div>
+        <div className="pt-20">
+          <Marquee gradient={false} speed={30}>
+            {brands.map((brand, i) => (
+              <motion.div
+                key={i}
+                whileHover={{ scale: 1.1 }}
+                className="mx-4 flex items-center"
+              >
+                <a href={brand.link} target="_blank" rel="noopener noreferrer">
+                  <div className="w-40 h-20 md:w-48 md:h-24 flex items-center justify-center">
+                    <img
+                      src={brand.logo}
+                      alt={brand.name}
+                      className="object-contain"
+                      style={
+                        brand.name === "Scale"
+                          ? { width: "80%", height: "80%" }
+                          : {}
+                      }
+                    />
+                  </div>
+                </a>
+              </motion.div>
+            ))}
+          </Marquee>
+          <div className="text-center opacity-80 font-medium pt-4 pb-8 text-xs md:text-lg">
+            Proudly Sponsored by Industry-Defining AI Companies
+          </div>
+        </div>
+      </LargeCallout>
+    </div>
+  );
+}
+
+export default Join;
