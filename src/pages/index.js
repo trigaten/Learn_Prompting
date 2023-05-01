25afe5a63d20881ee4332d28e741754c88e37de9
trigaten <sanderschulhoff@gmail.com>
2023-04-30 20:08:40 -0400

    fix: move subsections out of pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import "framer-motion";
 import "./index.css";
 import "./App.css";
-import Navbar from "./subsections/landingpage/Navbar";
-import Hero from "./subsections/landingpage/Hero";
-import Join from "./subsections/landingpage/Join";
-import FAQ from "./subsections/landingpage/FAQ";
-import Newsletter from "./subsections/landingpage/Newsletter";
-import GetStarted from "./subsections/landingpage/GetStarted";
+import Navbar from "../subsections/landingpage/Navbar";
+import Hero from "../subsections/landingpage/Hero";
+import Join from "../subsections/landingpage/Join";
+import FAQ from "../subsections/landingpage/FAQ";
+import Newsletter from "../subsections/landingpage/Newsletter";
+import GetStarted from "../subsections/landingpage/GetStarted";
 import Footer from "../components/Footer";
-import Banner from "./subsections/landingpage/Banner";
+import Banner from "../subsections/landingpage/Banner";
 
 function Home() {
   return (
