27b8b345a9c377169536cebd527abc7b8d6ede59
trigaten <sanderschulhoff@gmail.com>
2023-05-01 01:20:44 +0000

    deploy: 9ce40daf21d3d5392760427741dfcc14a8c95c6a

---
+++ fr/assets/js/d58f2f6c.72c67f9c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpromptgineering=self.webpackChunkpromptgineering||[]).push([[7800],{45369:e=>{e.exports=JSON.parse('{"permalink":"/fr/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
