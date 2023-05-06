5a5e49c955d723f68a5e7d2a56fd12dd2db3c240
anaumkhan <anaumkhan10@gmail.com>
2023-05-06 14:27:09 -0400

    fix: merge conflict

--- src/subsections/landingpage/Join.js
+++ src/subsections/landingpage/Join.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 import { motion } from "framer-motion";
-import ClassicPadding from "@site/src/components/layouts/ClassicPadding";
 import StatPoint from "@site/src/components/StatPoint";
 import LargeCallout from "@site/src/components/layouts/LargeCallout";
 import Button from "@site/src/components/Button";
@@ -60,13 +59,12 @@ function Join() {
         <span style={{ color: "black" }}>Join our global community.</span>
         <br />
         <span style={{ color: "black" }}>Elevate your prompting skills.</span>
-        <div className="text-center text-default text-sm font-vietnam font-light tracking-tight px-4 md:px-16 lg:px-32 xl:px-64 pt-2 md:pt-6 lg:pt-10">
+        <div className="text-center text-default text-sm font-vietnam font-light tracking-tight px-4 md:px-56 lg:px-32 xl:px-64 pt-2 md:pt-6 lg:pt-10">
           Become part of a worldwide network of learners from various
           industries, all mastering the skill of effectively engaging with AI
           using our curriculum
         </div>
       </div>
-
       <div className="flex flex-col gap-8 md:gap-0 md:flex-row items-center justify-between px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-32 text-transparent bg-clip-text bg-gradient-to-r from-[#005046] to-[#027F75] pt-14 pb-10 max-w-screen-xl mx-auto">
         {stats.map((stat, i) => (
           <StatPoint key={i} header={stat.header} subtitle={stat.subtitle} />
