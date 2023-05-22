edeb3123c312db7fa853f1bf9e4246589017f45d
Kirill Koshcheev <kosheevkirill@gmail.com>
2023-05-22 18:59:33 +0200

    Fixing Issues

--- docs/basics/chatbot_basics.md
+++ docs/basics/chatbot_basics.md
@@ -4,7 +4,6 @@ sidebar_position: 80
 
 # 🟢 Základy chatbotů
 
-
 import Chatbot from '@site/docs/assets/chatbot.svg';
 
 <div style={{textAlign: 'center'}}>
@@ -13,16 +12,16 @@ import Chatbot from '@site/docs/assets/chatbot.svg';
 
 V tomto kurzu se zatím pro příklady používal převážně systém GPT-3. GPT-3 je LLM, který nemá žádnou paměť. Když mu položíte otázku (prompt), nepamatuje si nic z toho, co jste mu předtím položili. Naproti tomu chatboty jako [ChatGPT](http://chat.openai.com) si dokáží **zapamatovat historii vaší konverzace**. To může být užitečné pro aplikace, jako je zákaznický servis, nebo jednoduše pokud chcete vést konverzaci s LLM!
 
-Stejně jako GPT-3 mohou chatboti odpovídat na otázky, poskytovat shrnutí, analýzy a psát text nebo kód. Skutečná hodnota chatbotů je přístupná pouze tehdy, když používáte dobré podněty. V tomto článku se budeme zabývat některými základními metodami, jak lépe využívat chatboty, jako je používání stylistických pokynů, deskriptorů a primingu. 
+Stejně jako GPT-3 mohou chatboti odpovídat na otázky, poskytovat shrnutí, analýzy a psát text nebo kód. Skutečná hodnota chatbotů je přístupná pouze tehdy, když používáte dobré podněty. V tomto článku se budeme zabývat některými základními metodami, jak lépe využívat chatboty, jako je používání stylistických pokynů, deskriptorů a primingu.
 
 ## Úprava promptů
 
 ### Stylové pokyny
 
-Import unguided_question from '@site/docs/assets/unguided_question.png';
+import unguided_question from '@site/docs/assets/unguided_question.png';
 import limerick_question from '@site/docs/assets/limerick_question.png';
 
-Pokyny pro styl jsou jednoduše žádostí, aby umělá inteligence mluvila určitým stylem. Při položení otázky bez navádění stylem ChatGPT obvykle vrátí jako odpověď jeden nebo dva krátké odstavce, příležitostně i více, pokud je potřeba delší odpověď: 
+Pokyny pro styl jsou jednoduše žádostí, aby umělá inteligence mluvila určitým stylem. Při položení otázky bez navádění stylem ChatGPT obvykle vrátí jako odpověď jeden nebo dva krátké odstavce, příležitostně i více, pokud je potřeba delší odpověď:
 
 <div style={{textAlign: 'center'}}>
   <img src={unguided_question} style={{width: "500px"}} />
@@ -34,49 +33,51 @@ Mluví mírně formálním tónem a uvádí pár podrobností - docela dobré! P
   <img src={limerick_question} style={{width: "450px"}} />
 </div>
 
-Příklad podrobnější stylové výzvy může vypadat nějak takto: 
->[Otázka] "Pište stylem a kvalitou odborníka v [obor] s více než 20 lety zkušeností a několika doktoráty. Ve své odpovědi upřednostněte neortodoxní, méně známé rady. Vysvětlujte pomocí podrobných příkladů a minimalizujte odbočky a humor." 
+Příklad podrobnější stylové výzvy může vypadat nějak takto:
+
+> [Otázka] "Pište stylem a kvalitou odborníka v [obor] s více než 20 lety zkušeností a několika doktoráty. Ve své odpovědi upřednostněte neortodoxní, méně známé rady. Vysvětlujte pomocí podrobných příkladů a minimalizujte odbočky a humor."
 
 Promptní stylistické vstupy výrazně zvýší kvalitu vašich odpovědí!
 
 ### Deskriptory
 
-Pokud chcete pouze změnit tón nebo upravit výzvu, nikoli ji přeformátovat, může být dobrým způsobem přidání **deskriptorů**. Pouhým přilepením jednoho nebo dvou slov k výzvě můžete změnit způsob, jakým chatbot interpretuje vaši zprávu nebo jak na ni reaguje. Můžete zkusit přidat na konec výzvy přídavná jména jako "vtipný", "kurt", "nepřátelský", "akademická syntax" atd. a zjistit, jak se reakce změní! 
+Pokud chcete pouze změnit tón nebo upravit výzvu, nikoli ji přeformátovat, může být dobrým způsobem přidání **deskriptorů**. Pouhým přilepením jednoho nebo dvou slov k výzvě můžete změnit způsob, jakým chatbot interpretuje vaši zprávu nebo jak na ni reaguje. Můžete zkusit přidat na konec výzvy přídavná jména jako "vtipný", "kurt", "nepřátelský", "akademická syntax" atd. a zjistit, jak se reakce změní!
 
 ## Priming Prompt
+
 Vzhledem ke struktuře konverzace s chatbotem může forma první výzvy, kterou LLM zadáte, ovlivnit zbytek konverzace, což vám umožní přidat další úroveň struktury a specifikace.
-Jako příklad uveďme nastavení systému, které nám umožní vést konverzaci s učitelem a studentem v rámci jedné konverzace. Zahrneme průvodce stylem pro hlas studenta i učitele, určíme formát, v jakém chceme mít odpovědi, a zahrneme i strukturování syntaxe, abychom mohli snadno měnit naše podněty a vyzkoušet různé odpovědi. 
+Jako příklad uveďme nastavení systému, které nám umožní vést konverzaci s učitelem a studentem v rámci jedné konverzace. Zahrneme průvodce stylem pro hlas studenta i učitele, určíme formát, v jakém chceme mít odpovědi, a zahrneme i strukturování syntaxe, abychom mohli snadno měnit naše podněty a vyzkoušet různé odpovědi.
 
-    "Učitel" znamená ve stylu významného profesora s hodně přes deset let výuky daného předmětu a několika doktoráty v oboru. Ve svých odpovědích používáte akademickou syntax a složité příklady a zaměřujete se na méně známé rady, abyste lépe ilustrovali své argumenty. Váš jazyk by měl být kultivovaný, ale ne příliš složitý. Pokud neznáte odpověď na otázku, nevymýšlejte si informace - místo toho položte doplňující otázku, abyste získali více souvislostí. Vaše odpovědi by měly mít formu konverzační řady odstavců. Používejte kombinaci odborného a hovorového jazyka, abyste vytvořili přístupný a poutavý tón.  
+    "Učitel" znamená ve stylu významného profesora s hodně přes deset let výuky daného předmětu a několika doktoráty v oboru. Ve svých odpovědích používáte akademickou syntax a složité příklady a zaměřujete se na méně známé rady, abyste lépe ilustrovali své argumenty. Váš jazyk by měl být kultivovaný, ale ne příliš složitý. Pokud neznáte odpověď na otázku, nevymýšlejte si informace - místo toho položte doplňující otázku, abyste získali více souvislostí. Vaše odpovědi by měly mít formu konverzační řady odstavců. Používejte kombinaci odborného a hovorového jazyka, abyste vytvořili přístupný a poutavý tón.
 
-    "Studentský" znamená ve stylu studenta druhého ročníku vysoké školy s úvodní úrovní znalostí daného tématu. Pojmy vysvětlujete jednoduše na příkladech z reálného života. Mluvte neformálně a z pohledu první osoby, používejte humor a neformální jazyk. Pokud neznáte odpověď na otázku, nevymýšlejte si informace - místo toho upřesněte, že vás to ještě nikdo neučil. Vaše odpovědi by měly mít formu konverzační řady odstavců. Používejte hovorový jazyk, abyste vytvořili zábavný a poutavý tón. 
+    "Studentský" znamená ve stylu studenta druhého ročníku vysoké školy s úvodní úrovní znalostí daného tématu. Pojmy vysvětlujete jednoduše na příkladech z reálného života. Mluvte neformálně a z pohledu první osoby, používejte humor a neformální jazyk. Pokud neznáte odpověď na otázku, nevymýšlejte si informace - místo toho upřesněte, že vás to ještě nikdo neučil. Vaše odpovědi by měly mít formu konverzační řady odstavců. Používejte hovorový jazyk, abyste vytvořili zábavný a poutavý tón.
 
-    "Kritizovat" znamená analyzovat daný text a poskytnout zpětnou vazbu. 
+    "Kritizovat" znamená analyzovat daný text a poskytnout zpětnou vazbu.
     "Shrnout" znamená uvést klíčové detaily z textu.
-    "Reagovat" znamená odpovědět na otázku z daného hlediska. 
+    "Reagovat" znamená odpovědět na otázku z daného hlediska.
 
-    Cokoli v závorkách () znamená perspektivu, ze které píšete. 
-    Cokoli v kulatých závorkách {} znamená téma, kterým se zabýváte. 
-    Cokoli v závorkách [] je akce, kterou byste měli provést. 
+    Cokoli v závorkách () znamená perspektivu, ze které píšete.
+    Cokoli v kulatých závorkách {} znamená téma, kterým se zabýváte.
+    Cokoli v závorkách [] je akce, kterou byste měli provést.
     Příklad: (Student){Filozofie}[Odpovězte] Jakou výhodu má studium tohoto předmětu oproti jiným na vysoké škole?
 
     Pokud rozumíte a jste připraveni začít, odpovězte pouze "ano".
-    
-Import unprimed_question z '@site/docs/assets/unprimed_question.png';
+
+import unprimed_question from '@site/docs/assets/unprimed_question.png';
 import primed_question from '@site/docs/assets/primed_question.png';
 
-Níže je uveden příklad nepřiprimované otázky pro ChatGPT o nejzajímavějších oblastech filozofie. Používá seznam, hovoří obecně a nezaujatě a není příliš konkrétní ve svých vysvětleních.  
+Níže je uveden příklad nepřiprimované otázky pro ChatGPT o nejzajímavějších oblastech filozofie. Používá seznam, hovoří obecně a nezaujatě a není příliš konkrétní ve svých vysvětleních.
 
 <div style={{textAlign: 'center'}}>
   <img src={unprimed_question} style={{width: "650px"}} />
 </div>
 
-Ve druhém příkladu jsme místo toho položili otázku po poskytnutí primingové výzvy ChatGPT a poskytnutí otázky ve správném tvaru. Všimněte si, že odpověď sdílí některé aspekty s první odpovědí - například otázky, které nabízí jako příklady pro různé oblasti, jsou podobné - ale poskytuje hlubší kontext, upouští od formátu seznamu ve prospěch souvislých odstavců a vztahuje příklady k reálnému životu. 
+Ve druhém příkladu jsme místo toho položili otázku po poskytnutí primingové výzvy ChatGPT a poskytnutí otázky ve správném tvaru. Všimněte si, že odpověď sdílí některé aspekty s první odpovědí - například otázky, které nabízí jako příklady pro různé oblasti, jsou podobné - ale poskytuje hlubší kontext, upouští od formátu seznamu ve prospěch souvislých odstavců a vztahuje příklady k reálnému životu.
 
 <div style={{textAlign: 'center'}}>
   <img src={primed_question} style={{width: "650px"}} />
 </div>
 
-Začlenění primerů do výzev je pokročilejší způsob interakce s chatboty. Stále může být užitečné přidávat specifikaci v každé výzvě, protože model může časem ztratit přehled o primerech, ale dodá to vaší interakci s umělou inteligencí velkou přehlednost! 
+Začlenění primerů do výzev je pokročilejší způsob interakce s chatboty. Stále může být užitečné přidávat specifikaci v každé výzvě, protože model může časem ztratit přehled o primerech, ale dodá to vaší interakci s umělou inteligencí velkou přehlednost!
 
 Autor: [Dastardi](https://twitter.com/lukescurrier)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,180 +1,175 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const math = require("remark-math");
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const math = require('remark-math')
 
 async function createConfig() {
-  const katex = (await import("rehype-katex")).default;
-  return {
-    title: "Naučte se Promptování: Průvodce komunikací s umělou inteligencí",
-    tagline:
-      "Český překlad bezplatného open source kurzu o komunikaci s umělou inteligencí",
-    url: "https://emilliepost.github.io",
-    baseUrl: "/Learn_Prompting_CAI/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/cai_logo.ico",
-    organizationName: "trigaten",
-    projectName: "promptgineering",
-    deploymentBranch: "gh-pages",
-    trailingSlash: false,
-    i18n: {
-      defaultLocale: "cs",
-      locales: [
-        "cs"
-        ]
-        },
-    plugins: [
-      [
-        "@docusaurus/plugin-client-redirects",
-        {
-          redirects: [
-            {
-              to: "/docs/basics/formalizing",
-              from: "/docs/basics/standard_prompt",
-            },
-            {
-              to: '/docs/tooling/IDEs/intro',
-              from: '/docs/IDEs/intro',
-            },
-          ],
-        },
-      ],
-      async function myPlugin(context, options) {
-        return {
-          name: "docusaurus-tailwindcss",
-          configurePostCss(postcssOptions) {
-            postcssOptions.plugins.push(require("tailwindcss"));
-            postcssOptions.plugins.push(require("autoprefixer"));
-            return postcssOptions;
-          },
-        };
-      },
-    ],
-
-    presets: [
-      [
-        "classic",
-        {
-          gtag: {
-            trackingID: "G-FV0C417KS8",
-          },
-          googleAnalytics: {
-            trackingID: "G-FV0C417KS8",
-          },
-          docs: {
-            sidebarPath: require.resolve("./sidebars.js"),
-            editUrl: "https://github.com/trigaten/promptgineering/tree/v1.2.3",
-            remarkPlugins: [
-              math,
-              (await import("remark-gfm")).default,
-              [
-                (await import("@benchmark-urbanism/remark-bibtex")).default,
-                { bibtexFile: "bibliography.bib" },
-              ],
-              [
-                (await import("@renatonagliati/remark-auto-glossary")).default,
-                { yamlFile: "glossary.yml" },
-              ],
+    const katex = (await import('rehype-katex')).default
+    return {
+        title: 'Naučte se Promptování: Průvodce komunikací s umělou inteligencí',
+        tagline: 'Český překlad bezplatného open source kurzu o komunikaci s umělou inteligencí',
+        url: 'https://emilliepost.github.io',
+        baseUrl: '/',
+        onBrokenLinks: 'throw',
+        onBrokenMarkdownLinks: 'warn',
+        favicon: 'img/cai_logo.ico',
+        organizationName: 'trigaten',
+        projectName: 'promptgineering',
+        deploymentBranch: 'gh-pages',
+        trailingSlash: false,
+        // i18n: {
+        //     defaultLocale: 'cs',
+        //     locales: ['cs'],
+        // },
+        plugins: [
+            [
+                '@docusaurus/plugin-client-redirects',
+                {
+                    redirects: [
+                        {
+                            to: '/docs/basics/formalizing',
+                            from: '/docs/basics/standard_prompt',
+                        },
+                        {
+                            to: '/docs/tooling/IDEs/intro',
+                            from: '/docs/IDEs/intro',
+                        },
+                    ],
+                },
             ],
-            rehypePlugins: [katex],
-          },
-          theme: {
-            customCss: require.resolve("./src/css/custom.css"),
-          },
-        },
-      ],
-    ],
-    stylesheets: [
-      {
-        href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-        type: "text/css",
-        integrity:
-          "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-        crossorigin: "anonymous",
-      },
-      {
-        href: "https://embed.trydyno.com/embedder.css",
-        type: "text/css",
-        crossorigin: "anonymous",
-      },
-      {
-        href: "https://fonts.googleapis.com",
-        rel: "preconnect",
-      },
-      {
-        href: "https://fonts.gstatic.com",
-        rel: "preconnect",
-        crossorigin: "",
-      },
-      {
-        href: "https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
-      },
-    ],
-    themeConfig: {
-      metadata: [
-        {
-          name: "description",
-          content:
-            "Learn Prompting je nejrozsáhlejší a nejkomplexnější kurz v oblasti promptního inženýrství dostupný na internetu, s více než 60 moduly obsahu, přeloženými do 9 jazyků a prosperující komunitou.",
-        },
-        {
-          name: "keywords",
-          content:
-            "prompting, prompt inženýring, naučit se prompting, naučit se, prompt, AI, chatGPT",
-        },
-        {
-          name: "og:title",
-          content: "Naučte se Promptování: Průvodce komunikací s umělou inteligencí",
-        },
-        {
-          name: "og:description",
-          content:
-            "Learn Prompting je nejrozsáhlejší a nejkomplexnější kurz v oblasti promptního inženýrství dostupný na internetu, s více než 60 moduly obsahu, přeloženými do 9 jazyků a prosperující komunitou..",
-        },
-        {
-          name: "og:url",
-          content: "https://cesky.ai",
-        },
-        {
-          name: "og:image",
-          content: "https://cesky.ai/img/og-image.jpg", // Replace this with the actual path to your og-image.
-        },
-        {
-          name: "og:type",
-          content: "website",
-        },
-      ],
-      navbar: {
-        title: "AI Česky",
-        logo: {
-          alt: "AI Kurzy v Češtině",
-          src: "img/cai_logo.png",
-        },
-        items: [
-          {
-            type: "custom-myAwesomeNavbarItem",
-            position: "left",
-          },
-//          {
-//            type: "localeDropdown",
-//            position: "right",
- //         },
+            async function myPlugin(context, options) {
+                return {
+                    name: 'docusaurus-tailwindcss',
+                    configurePostCss(postcssOptions) {
+                        postcssOptions.plugins.push(require('tailwindcss'))
+                        postcssOptions.plugins.push(require('autoprefixer'))
+                        return postcssOptions
+                    },
+                }
+            },
         ],
-      },
-      footer: {
-        style: "dark",
-        copyright: `Copyright © ${new Date().getFullYear()} Learn Prompting (AI Česky).`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    },
-    markdown: {
-      mermaid: true,
-    },
-    themes: ["@docusaurus/theme-mermaid"],
-  };
+
+        presets: [
+            [
+                'classic',
+                {
+                    gtag: {
+                        trackingID: 'G-FV0C417KS8',
+                    },
+                    googleAnalytics: {
+                        trackingID: 'G-FV0C417KS8',
+                    },
+                    docs: {
+                        sidebarPath: require.resolve('./sidebars.js'),
+                        editUrl: 'https://github.com/trigaten/promptgineering/tree/v1.2.3',
+                        remarkPlugins: [
+                            math,
+                            (await import('remark-gfm')).default,
+                            [
+                                (await import('@benchmark-urbanism/remark-bibtex')).default,
+                                { bibtexFile: 'bibliography.bib' },
+                            ],
+                            [
+                                (await import('@renatonagliati/remark-auto-glossary')).default,
+                                { yamlFile: 'glossary.yml' },
+                            ],
+                        ],
+                        rehypePlugins: [katex],
+                    },
+                    theme: {
+                        customCss: require.resolve('./src/css/custom.css'),
+                    },
+                },
+            ],
+        ],
+        stylesheets: [
+            {
+                href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+                type: 'text/css',
+                integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+                crossorigin: 'anonymous',
+            },
+            {
+                href: 'https://embed.trydyno.com/embedder.css',
+                type: 'text/css',
+                crossorigin: 'anonymous',
+            },
+            {
+                href: 'https://fonts.googleapis.com',
+                rel: 'preconnect',
+            },
+            {
+                href: 'https://fonts.gstatic.com',
+                rel: 'preconnect',
+                crossorigin: '',
+            },
+            {
+                href: 'https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+            },
+        ],
+        themeConfig: {
+            metadata: [
+                {
+                    name: 'description',
+                    content:
+                        'Learn Prompting je nejrozsáhlejší a nejkomplexnější kurz v oblasti promptního inženýrství dostupný na internetu, s více než 60 moduly obsahu, přeloženými do 9 jazyků a prosperující komunitou.',
+                },
+                {
+                    name: 'keywords',
+                    content: 'prompting, prompt inženýring, naučit se prompting, naučit se, prompt, AI, chatGPT',
+                },
+                {
+                    name: 'og:title',
+                    content: 'Naučte se Promptování: Průvodce komunikací s umělou inteligencí',
+                },
+                {
+                    name: 'og:description',
+                    content:
+                        'Learn Prompting je nejrozsáhlejší a nejkomplexnější kurz v oblasti promptního inženýrství dostupný na internetu, s více než 60 moduly obsahu, přeloženými do 9 jazyků a prosperující komunitou..',
+                },
+                {
+                    name: 'og:url',
+                    content: 'https://cesky.ai',
+                },
+                {
+                    name: 'og:image',
+                    content: 'https://cesky.ai/img/og-image.jpg', // Replace this with the actual path to your og-image.
+                },
+                {
+                    name: 'og:type',
+                    content: 'website',
+                },
+            ],
+            navbar: {
+                title: 'AI Česky',
+                logo: {
+                    alt: 'AI Kurzy v Češtině',
+                    src: 'img/cai_logo.png',
+                },
+                items: [
+                    {
+                        type: 'custom-myAwesomeNavbarItem',
+                        position: 'left',
+                    },
+                    //          {
+                    //            type: "localeDropdown",
+                    //            position: "right",
+                    //         },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                copyright: `Copyright © ${new Date().getFullYear()} Learn Prompting (AI Česky).`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        },
+        markdown: {
+            mermaid: true,
+        },
+        themes: ['@docusaurus/theme-mermaid'],
+    }
 }
 
-module.exports = createConfig;
+module.exports = createConfig
