a2ee392298be96d68138c08f5083f5316a8ad642
trigaten <sanderschulhoff@gmail.com>
2023-07-15 00:00:50 -0400

    feat: more about

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,7 +21,7 @@ function Home() {
         <Navbar />
         <div className="mx-32 my-10">
           <div className="relative">
-            <h1 className="text-5xl font-bold mb-10 text-center">Our mission is to elucidate Generative AI</h1>
+            <h1 className="text-5xl font-bold mb-10 text-center">Our Mission is to Elucidate Generative AI</h1>
             {circles.map((_, i) => (
               <div
                 key={i}
@@ -35,22 +35,31 @@ function Home() {
             ))}
           </div>
           <div className="grid grid-cols-3 gap-10">
-            <div className="text-center">
-              <img src={require("@site/static/team/sander.jpeg").default} alt="CEO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg"/>
-              <h2 className="text-xl font-semibold mt-2">CEO</h2>
+          <div className="text-center">
+              <img src={require("@site/static/team/sander.jpeg").default} alt="CTO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg" style={{ width: "200px", height: "200px" }}/>
+              <h2 className="text-xl font-semibold mt-2">Sander Schulhoff, CTO</h2>
             </div>
             <div className="text-center">
-              <img src={require("@site/static/team/sander.jpeg").default} alt="COO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg"/>
-              <h2 className="text-xl font-semibold mt-2">COO</h2>
+              <img src={require("@site/static/team/anaum.jpeg").default} alt="CEO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg" style={{ width: "200px", height: "200px" }}/>
+              <h2 className="text-xl font-semibold mt-2">Anaum Khan, CEO</h2>
             </div>
             <div className="text-center">
-              <img src={require("@site/static/team/sander.jpeg").default} alt="CTO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg"/>
-              <h2 className="text-xl font-semibold mt-2">CTO</h2>
+              <img src={require("@site/static/team/fady.jpeg").default} alt="COO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg" style={{ width: "200px", height: "200px" }}/>
+              <h2 className="text-xl font-semibold mt-2">Fady Yanni, COO</h2>
             </div>
           </div>
-          <p className="mt-10">
-            We are a platform for teaching and learning about prompt engineering. We maintain the most robust, beginner friendly course of prompt engineering.
+          <p className="mx-auto px-64 py-10">
+            We teach global audiences prompt engineering and generative AI. We also conduct large scale research on the topic. We are a team of 3, and we are based in the United States.
           </p>
+
+          <hr></hr>
+
+          <div className="grid grid-cols-3 gap-10">
+            <div className="text-center">
+                We also have these open source contributors
+            </div>
+          </div>
+
         </div>
         <Footer />
       </div>
