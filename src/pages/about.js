d95acf5f859a887ed1e4a050c4a8a93f35d3347c
trigaten <sanderschulhoff@gmail.com>
2023-07-16 16:58:28 -0400

    feat: about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,65 +1,56 @@
-import React, { useEffect, useState } from "react";
-import "framer-motion";
+import React from "react";
 import "./index.css";
 import "./App.css";
 import Navbar from "../subsections/landingpage/Navbar";
 import Footer from "../components/Footer";
 
 function Home() {
-  const [circles, setCircles] = useState(Array.from({ length: 100 }));
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCircles((prevCircles) => prevCircles.slice(1));
-    }, 100);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <>
-      <div className="bg-gray-300 relative overflow-hidden">
+      <div className="">
         <Navbar />
-        <div className="mx-32 my-10">
-          <div className="relative">
-            <h1 className="text-5xl font-bold mb-10 text-center">Our Mission is to Elucidate Generative AI</h1>
-            {circles.map((_, i) => (
-              <div
-                key={i}
-                className="fading-circle"
-                style={{
-                  top: `${Math.random() * 100}%`,
-                  left: `${Math.random() * 100}%`,
-                  animation: `fade 2s ${i * 0.1}s forwards`,
-                }}
-              />
-            ))}
-          </div>
-          <div className="grid grid-cols-3 gap-10">
+        <div className="container mx-auto py-10">
           <div className="text-center">
-              <img src={require("@site/static/team/sander.jpeg").default} alt="CTO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg" style={{ width: "200px", height: "200px" }}/>
-              <h2 className="text-xl font-semibold mt-2">Sander Schulhoff, CTO</h2>
-            </div>
-            <div className="text-center">
-              <img src={require("@site/static/team/anaum.jpeg").default} alt="CEO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg" style={{ width: "200px", height: "200px" }}/>
-              <h2 className="text-xl font-semibold mt-2">Anaum Khan, CEO</h2>
-            </div>
-            <div className="text-center">
-              <img src={require("@site/static/team/fady.jpeg").default} alt="COO" className="mx-auto rounded-full border-2 border-gray-300 shadow-lg" style={{ width: "200px", height: "200px" }}/>
-              <h2 className="text-xl font-semibold mt-2">Fady Yanni, COO</h2>
-            </div>
+            <h1 className="text-5xl md:text-5xl font-bold mb-8">Illuminate Generative AI</h1>
           </div>
-          <p className="mx-auto px-64 py-10">
-            We teach global audiences prompt engineering and generative AI. We also conduct large scale research on the topic. We are a team of 3, and we are based in the United States.
+          <p className="mx-auto px-8 md:px-32 text-lg text-gray-600 leading-relaxed text-center">
+            Our mission is to teach global audiences about prompt engineering and generative AI. 
           </p>
-
-          <hr></hr>
-
-          <div className="grid grid-cols-3 gap-10">
-            <div className="text-center">
-                We also have these open source contributors
+          <div className="bg-white shadow-lg rounded-lg p-8 my-10">
+            <h2 className="text-2xl md:text-4xl font-semibold mb-6">Our Team</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+              <div className="text-center">
+                <img src={require("@site/static/team/sander.jpeg").default} alt="CTO" className="mx-auto rounded-full border-4 border-gray-200 shadow-lg" style={{ width: "200px", height: "200px" }}/>
+                <h2 className="text-lg md:text-xl font-semibold mt-4">Sander Schulhoff</h2>
+                <p className="text-gray-500 mt-2">CTO</p>
+              </div>
+              <div className="text-center">
+                <img src={require("@site/static/team/anaum.jpeg").default} alt="CEO" className="mx-auto rounded-full  shadow-lg" style={{ width: "200px", height: "200px" }}/>
+                <h2 className="text-lg md:text-xl font-semibold mt-4">Anaum Khan</h2>
+                <p className="text-gray-500 mt-2">CEO</p>
+              </div>
+              <div className="text-center">
+                <img src={require("@site/static/team/fady.jpeg").default} alt="COO" className="mx-auto rounded-full border-4 border-gray-200 shadow-lg" style={{ width: "200px", height: "200px" }}/>
+                <h2 className="text-lg md:text-xl font-semibold mt-4">Fady Yanni</h2>
+                <p className="text-gray-500 mt-2">COO</p>
+              </div>
             </div>
           </div>
-
+          <div className="bg-white shadow-lg rounded-lg p-8 my-10">
+            <h2 className="text-2xl md:text-4xl font-semibold mb-6">Our Research</h2>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              We conduct large-scale research on the topic, such as the recent large language model security challenge, <a href="https://www.aicrowd.com/challenges/hackaprompt-2023">HackAPrompt</a>.
+            </p>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-8 my-10">
+            <h2 className="text-2xl md:text-4xl font-semibold mb-6">Open Source Software</h2>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              We maintain multiple Open Source projects in the large language model space. 
+              Our best known project is Learn Prompting, the most robust, beginner friendly guide
+              on prompt engineering.
+            </p>
+          </div>
+          
         </div>
         <Footer />
       </div>
