0a5d455b9978414e296fbe04bd637de9d578051c
trigaten <sanderschulhoff@gmail.com>
2023-07-19 15:42:10 -0400

    feat: all lazy

--- scripts/convert_to_lazy.py
+++ scripts/convert_to_lazy.py
@@ -3,20 +3,21 @@ import fileinput
 import re
 
 # Define the directory you want to start from
-rootDir = '../docs'
+rootDir = '../'
 
 for dirName, subdirList, fileList in os.walk(rootDir):
-    for fname in fileList:
-        if fname.endswith('.md'):
-            file_path = os.path.join(dirName, fname)
-            with fileinput.FileInput(file_path, inplace=True) as file:
-                for line in file:
-                    # Find img tag
-                    img_tag = re.search(r'<img (.+?) />', line)
-                    if img_tag:
-                        # Extract attributes
-                        attributes = img_tag.group(1)
-                        # Replace with LazyLoadImage component
-                        print(line.replace(img_tag.group(), f'<LazyLoadImage {attributes} />'), end='')
-                    else:
-                        print(line, end='')
+    if "node_modules" not in dirName:
+        for fname in fileList:
+            if fname.endswith('.md') or fname.endswith(".js"):
+                file_path = os.path.join(dirName, fname)
+                with fileinput.FileInput(file_path, inplace=True) as file:
+                    for line in file:
+                        # Find img tag
+                        img_tag = re.search(r'<img (.+?) />', line)
+                        if img_tag:
+                            # Extract attributes
+                            attributes = img_tag.group(1)
+                            # Replace with LazyLoadImage component
+                            print(line.replace(img_tag.group(), f'<LazyLoadImage {attributes} />'), end='')
+                        else:
+                            print(line, end='')
