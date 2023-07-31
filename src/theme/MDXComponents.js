ad3d126c07a3c460cd88f068f4076c678b676059
trigaten <sanderschulhoff@gmail.com>
2023-07-31 12:29:04 +0300

    docs: clean up

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -5,7 +5,5 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  // Map the "<Highlight>" tag to our Highlight component
-  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   LazyLoadImage,
 };
