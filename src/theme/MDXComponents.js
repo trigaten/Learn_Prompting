13cc61a74d82317ab23453b8f4fb37dd1a672b25
trigaten <sanderschulhoff@gmail.com>
2023-07-31 12:28:04 +0300

    fix: admonition rendering

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,3 +1,11 @@
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-export default {LazyLoadImage};
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  // Map the "<Highlight>" tag to our Highlight component
+  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
+  LazyLoadImage,
+};
