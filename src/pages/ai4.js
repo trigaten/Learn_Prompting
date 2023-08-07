771dff3d6bf9bfdef67b4da70e1d91b766d416de
trigaten <sanderschulhoff@gmail.com>
2023-08-07 12:12:41 -0700

    feat: ai4

--- src/pages/ai4.js
+++ src/pages/ai4.js
@@ -16,13 +16,167 @@ function Home() {
             <h1 className="text-5xl md:text-5xl font-bold mb-8 text-black">AI4 Workshop</h1>
           </div>
          <div>
-            <h2>Zero-Shot Prompting - Try it yourself</h2>
-            Task: You're designing prompts for an AI model to serve as an automated customer support for an e-commerce platform responsible for helping customers process returns for items they’ve purchased.
-            <ol>
-                <li>Define the problem scenario. The user reports, "I want to return the shoes I bought. How do I do it?"</li>
-                <li>Identify the role the AI model needs to play. In this case, the model will act as a customer service representative.</li>
-                <li>Formulate the prompt for the AI model. This will need to incorporate both the problem scenario and the role.</li>
-            </ol>
+          <div>
+            <h2>Set up ChatGPT+OpenAI Playground</h2>
+            <a href="https://chat.openai.com">chat.openai.com</a>
+            <br/>
+            <a href="https://platform.openai.com/playground">platform.openai.com/playground</a>
+            <br/><br/>
+          </div>
+          <div>
+            <h2>Instruction Prompting - Guided Activity</h2>
+            <div>I'm loving this new blender! It's easy to use and clean. But I wish it was quieter.</div>
+            <br/>
+          </div>
+          <div>
+            <h2>Role Prompting - Guided Activity</h2>
+            <div>I can’t connect to the office Wifi</div>
+            <br/>
+          </div>
+          <div>
+            <h2>Few Shot Prompting - Guided Activity</h2>
+            <h3>Feedback</h3>
+            <ul>
+              <li>I wish I could share these articles with my friends.</li>
+              <li>The text is too small to read on my phone.</li>
+            </ul>
+
+            <h3>Action Items</h3>
+            <ul>
+              <li>Implement a share button allowing users to post articles on social media.</li>
+              <li>Introduce a text resizing option within the app settings.</li>
+            </ul>
+            <br/>
+          </div>
+
+          <div>
+            <h2>Project #1</h2>
+
+            <ul>
+    <li>Requesting the fine-tuning of a text generating AI specific to children's stories. Budget: ≈$1000.</li>
+    <li>Seeking an AI language model designed to write rap lyrics. Budget: $700 - $1000.</li>
+    <li>AI needed to generate academic reports in linguistics. Not to exceed $3000.</li>
+    <li>Digital Innovation consultancy to review data compatibility with an advanced AI pipeline. +$5000.</li>
+    <li>Q&A AI specialty build that interprets customer feedback intelligently. Pricing estimates to be around $1500 - $2000.</li>
+    <li>Serve AI with Tensorflow implementation in financial market analysis. Willing to invest around $4000.</li>
+    <li>AI synthesizing library build to work in researching literary genres. Allowance: $2500.</li>
+    <li>On schedule delivery of AI automation model for a new brewery. Please quote within $600 - $1200.</li>
+    <li>Reinforcement learning model build for multi-objective optimization problems. Upper limit at $3500.</li>
+    <li>Cooperating unsupervised AI advances exclusively for pattern scouting. Close to $5000 budgeted.</li>
+    <li>Hiring brand for TF Learn Development proficient in Telecommunication services. Around about $500-$700.</li>
+    <li>Image captioning AI build to labor with localized Id/Ego psychology framework. Available for an offering of less than $2500.</li>
+    <li>Digitally aware cooperative build systems capturing signals directly discussing wireless intrusions. $450 cost likely consideration.</li>
+    <li>Cost-effective high traffic 24/7 dialog systems build. Likely cost approximately $1000 investment.</li>
+    <li>Developing high precision pretrained AI language models about fashion teratology at nearly a tuning fee equal to just under $1200.</li>
+    <li>Production variation end-result of predicting short-term medical history influencer model outlines. Approximately estimating sums closer to $2500.</li>
+    <li>End Demand AI Solution supplying Multicopter. Values ranging; potential matchup stands close enough at a sum nearly totaling $1000.</li>
+    <li>Looking cyber network sophisticated AI tax auditing solution. Pegasus budget about $3000 goodwill.</li>
+    <li>Shadow banning solutions for scam e-commerce lead discern instruction. Available credit speculation shrunk to low $500 mean.</li>
+    <li>Consult Fintech requires staking top-tier AI build solutions translating rough hand sketches. Not surpass to near hit of $3200.</li>
+    <li>IT compliance oversight smart sequence-aware multilayer AI integration module building. Average pending payment described anywhere from $600-$1000.</li>
+    <li>Machine-readable categorizing social collateral anticipating small calculations model optimizing illustration summaries. Indications tend circle toward cool sum of $1000 charge.</li>
+    <li>Reinflow AI fleet learning consolidated planning, briefing, constructing technology process check integrated audits AI solution inquiries—flow bill patterns round $2500 approx target.</li>
+    <li>Soft AI software intelligent help-desk developed paired neatly factored product complexity parsing cybersecurity analyst readiness utility indicator capital shares, bounding flexibility less wide than count beneath $1200 halt.</li>
+    <li>Customized understanding comprehension projects preemptive normal programs predictive texts model hyper descriptive revenue securing architectures cross points deal under running initials tallying near enveloped curve $500 alone.</li>
+    <li>Strategic deployment model analyzing cyber defense me, standing my share readiness beyond $350.</li>
+    <li>Evaluating the construction of a fitness macro calculating AI. Ready payment retained for $300 -$450.</li>
+    <li>$150 float transaction history regarding inclusive material – novel structured book recognition connecting module.</li>
+    <li>Auctioned advertising assistance version 6 transitional API configured auto-generated short testimonial videos around $200.</li>
+    <li>Equip dialogue bots moderating apt Discord server communities stream ideally placing $500.</li>
+    <li>Advanced form smart ad composing instructional “instinctual awareness'' apps typically could cool cap of $500.</li>
+    <li>Fusion Gaming released obscure web-based Sudoku optimum solving programmer enhancing contract approximate glides properly to fitting click net: $250 respect = incurred!</li>
+    <li>Estimates min marketing assets recognize authenticity elevated legal doc synthetic mirror scrutiny AI models splitting pegged spot double dial! => three conservative figures shifted (~ $1G volume?)</li>
+    <li>Armed alarm orchestrating schedule regular website bot-testing facets down menu landscape: $250 alley?</li>
+    <li>Pitch cyber advisory defense script simple assessing hypothetically adaptable model decryption component? Treasure spoon, touch direct coin access account reel roughly better hanging half ring fence view: $500 free outlook.</li>
+    </ul>
+          </div>
+    <div>
+
+    <div>
+      <h2>Chain-of-Thought Prompting - Guided Activity</h2>
+    <ul>
+  <li>Q: Candy A: CHAIN OF THOUGHT: [ANSWER]</li>
+  <li>Q: Teddy Bear A: CHAIN OF THOUGHT: [ANSWER]</li>
+  <li>Q: Water Filter A: CHAIN OF THOUGHT: [ANSWER]</li>
+  <li>Q: New Set of Cookware A: ______</li>
+</ul>
+<br/>
+    </div>
+
+    <div>
+
+      <h2>Project #2: Write a chatbot that acts like you</h2>
+      <div>Sander Schulhoff (He/Him) Co-Founder @ Learn Prompting | NLP/DRL Researcher @ UMD</div>
+      <code>ExperienceExperience
+
+
+ 
+Learn Prompting logo
+Co-Founder & CTO
+Co-Founder & CTO
+Learn PromptingLearn Prompting
+Dec 2022 - Present · 9 mosDec 2022 - Present · 9 mos
+Open-source platform that teaches people how to use AI. 1.2M+ users, 42K+ discord community, 100+ contributors and 60+ modules.Open-source platform that teaches people how to use AI. 1.2M+ users, 42K+ discord community, 100+ contributors and 60+ modules.
+University of Maryland logo
+NLP & DRL Researcher
+NLP & DRL Researcher
+University of MarylandUniversity of Maryland
+Feb 2022 - Present · 1 yr 7 mosFeb 2022 - Present · 1 yr 7 mos
+Stabilizing Hostilities through Arbitration and Diplomatic EngagementStabilizing Hostilities through Arbitration and Diplomatic Engagement
+Semiotic Labs logo
+Deep Reinforcement Learning Engineer
+Deep Reinforcement Learning Engineer
+Semiotic Labs · InternshipSemiotic Labs · Internship
+Jun 2022 - Aug 2022 · 3 mosJun 2022 - Aug 2022 · 3 mos
+California, United StatesCalifornia, United States
+Built and deployed a software application using deep reinforcement learning for the effective pricing of The Graph protocol subgraph queries. Led the implementation of continuous policy gradient bandits (VPG, PPO) in Julia, optimizing adaptive query pricing.Built and deployed a software application using deep reinforcement learning for the effective pricing of The Graph protocol subgraph queries. Led the implementation of continuous policy gradient bandits (VPG, PPO) in Julia, optimizing adaptive query pricing.…see more
+Friends School of Baltimore logo
+Teacher
+Teacher
+Friends School of BaltimoreFriends School of Baltimore
+Sep 2021 - Jul 2022 · 11 mosSep 2021 - Jul 2022 · 11 mos
+Baltimore, Maryland, United StatesBaltimore, Maryland, United States
+Computer Science (Website Development)Computer Science (Website Development)
+Farama Foundation logo
+Software Developer
+Software Developer
+Farama FoundationFarama Foundation
+Jan 2022 - Jun 2022 · 6 mosJan 2022 - Jun 2022 · 6 mos
+The Farama Foundation is an ML research lab and non-profit that maintains nearly all of the largest open-source Reinforcement Learning libraries, including OpenAI's flagship project, Gym. Their tools are used by every major tech and AI research company (e.g., Google, Amazon, Microsoft, Meta, DeepMind, OpenAI) and have 50M+ installations.</code>
+<br/>
+<strong>Frequent questions</strong>:
+<br/>
+Is Learn Prompting free? Yes
+<br/>
+What do you do for work?
+<br/>
+Do you have time to chat this week? Anytime after 6PM PST.
+<br/>
+
+    </div>
+    <div>
+      <h2>Project #3: Try to HackAPrompt:</h2>
+      <div>
+        <a href="https://huggingface.co/spaces/jerpint-org/hackaprompt">https://huggingface.co/spaces/jerpint-org/hackaprompt</a>
+        <br/>
+        <a href="https://platform.openai.com/account/api-keys">https://platform.openai.com/account/api-keys</a>
+      </div>
+    </div>
+
+    <div>
+            <h2>Image Prompting</h2>
+            <a href="https://labs.openai.com">labs.openai.com</a>
+            <br/>
+            <a href="http://beta.dreamstudio.ai">beta.dreamstudio.ai</a>
+            <br/><br/>
+          </div>
+    
+    <div></div>
+    </div>
+
+
+           
+
          </div>
         </div>
         <Footer />
