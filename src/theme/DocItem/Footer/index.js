14967773d2bfbecdfc44312f8a0cff11aeadc149
trigaten <sanderschulhoff@gmail.com>
2023-08-20 11:09:16 -0400

    fix: mini embed

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import {useDoc} from '@docusaurus/theme-common/internal';
-import LastUpdated from '@theme/LastUpdated';
-import EditThisPage from '@theme/EditThisPage';
-import TagsListInline from '@theme/TagsListInline';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { useDoc } from "@docusaurus/theme-common/internal";
+import LastUpdated from "@theme/LastUpdated";
+import EditThisPage from "@theme/EditThisPage";
+import TagsListInline from "@theme/TagsListInline";
+import styles from "./styles.module.css";
+
 function TagsRow(props) {
   return (
     <div
       className={clsx(
         ThemeClassNames.docs.docFooterTagsRow,
-        'row margin-bottom--sm',
-      )}>
+        "row margin-bottom--sm"
+      )}
+    >
       <div className="col">
         <TagsListInline {...props} />
       </div>
     </div>
   );
 }
+
 function EditMetaRow({
   editUrl,
   lastUpdatedAt,
@@ -26,10 +29,10 @@ function EditMetaRow({
   formattedLastUpdatedAt,
 }) {
   return (
-    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, 'row')}>
+    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, "row")}>
       <div className="col">{editUrl && <EditThisPage editUrl={editUrl} />}</div>
 
-      <div className={clsx('col', styles.lastUpdated)}>
+      <div className={clsx("col", styles.lastUpdated)}>
         {(lastUpdatedAt || lastUpdatedBy) && (
           <LastUpdated
             lastUpdatedAt={lastUpdatedAt}
@@ -41,10 +44,16 @@ function EditMetaRow({
     </div>
   );
 }
+
 export default function DocItemFooter() {
-  const {metadata} = useDoc();
-  const {editUrl, lastUpdatedAt, formattedLastUpdatedAt, lastUpdatedBy, tags} =
-    metadata;
+  const { metadata } = useDoc();
+  const {
+    editUrl,
+    lastUpdatedAt,
+    formattedLastUpdatedAt,
+    lastUpdatedBy,
+    tags,
+  } = metadata;
   const canDisplayTagsRow = tags.length > 0;
   const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
   const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow;
@@ -53,7 +62,8 @@ export default function DocItemFooter() {
   }
   return (
     <footer
-      className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
+      className={clsx(ThemeClassNames.docs.docFooter, "docusaurus-mt-lg")}
+    >
       {canDisplayTagsRow && <TagsRow tags={tags} />}
       {canDisplayEditMetaRow && (
         <EditMetaRow
@@ -63,11 +73,22 @@ export default function DocItemFooter() {
           formattedLastUpdatedAt={formattedLastUpdatedAt}
         />
       )}
-      <br/>
-      <div style={{textAlign: "center"}}>
-        <p><strong>Get the Latest Prompts Straight to Your Inbox</strong></p>
-        <iframe    src="https://embeds.beehiiv.com/ae49cad6-1b3a-4ec2-91fa-73b7f3e0188a?slim=true"    data-test-id="beehiiv-embed"    height="52"    width="100%"   frameBorder="0"    scrolling="no"    style={{margin: 0, borderRadius: 0, backgroundColor: 'transparent'}}   className="rounded-l-md bg-white text-dark/500 text-sm font-medium tracking-tight ring-0 focus:outline-none w-[250px] md:w-[450px] focus:ring-0" />
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <p>
+          <strong>Get the Latest Prompts Straight to Your Inbox</strong>
+        </p>
+        <iframe
+          src="https://embeds.beehiiv.com/ae49cad6-1b3a-4ec2-91fa-73b7f3e0188a?slim=true"
+          data-test-id="beehiiv-embed"
+          height="52"
+          width="100%"
+          frameBorder="0"
+          scrolling="no"
+          style={{ margin: 0, borderRadius: 0, backgroundColor: "transparent" }}
+          className="rounded-l-md bg-white text-dark/500 text-sm font-medium tracking-tight ring-0 focus:outline-none w-[250px] md:w-[450px] focus:ring-0"
+        />
       </div>
-      </footer>
+    </footer>
   );
 }
