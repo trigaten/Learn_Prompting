f3e59c4afb6f973ef26630564ad0bd8bebb768b8
trigaten <sanderschulhoff@gmail.com>
2023-11-30 17:39:53 -0500

    feat: new logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,45 @@
-import React from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import "framer-motion";
-import "./index.css";
-import "./App.css";
-import Navbar from "../subsections/landingpage/Navbar";
-import Hero from "../subsections/landingpage/Hero";
-import Join from "../subsections/landingpage/Join";
-import FAQ from "../subsections/landingpage/FAQ";
-import Newsletter from "../subsections/landingpage/Newsletter";
-import GetStarted from "../subsections/landingpage/GetStarted";
-import Footer from "../components/Footer";
-import Banner from "../subsections/landingpage/Banner";
+// import React from "react";
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import "framer-motion";
+// import "./index.css";
+// import "./App.css";
+// import Navbar from "../subsections/landingpage/Navbar";
+// import Hero from "../subsections/landingpage/Hero";
+// import Join from "../subsections/landingpage/Join";
+// import FAQ from "../subsections/landingpage/FAQ";
+// import Newsletter from "../subsections/landingpage/Newsletter";
+// import GetStarted from "../subsections/landingpage/GetStarted";
+// import Footer from "../components/Footer";
+// import Banner from "../subsections/landingpage/Banner";
 
-function Home() {
-  return (
-    <>
-      {/*<Banner />*/}
-      <div className="bg-white">
-        <Navbar allowSandwich="true"/>
-        <Hero />
-        <Join />
-        <FAQ />
-        <Newsletter />
-        <GetStarted />
-        <Footer />
-      </div>
-    </>
-  );
-}
+// function Home() {
+//   return (
+//     <>
+//       {/*<Banner />*/}
+//       <div className="bg-white">
+//         <Navbar allowSandwich="true"/>
+//         <Hero />
+//         <Join />
+//         <FAQ />
+//         <Newsletter />
+//         <GetStarted />
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// }
 
-export default Home;
+// export default Home;
+import React, { useEffect } from 'react';
+import Link from '@docusaurus/Link';
+
+export default function Home() {
+    let url = "https://learnprompting.org"
+    useEffect(() => {
+        window.location.href = url;
+    }, []);
+
+    return (
+        <p>Redirecting to link..., please click <Link href={url} target="_self">here</Link> if not redirected</p>
+    );
+};
